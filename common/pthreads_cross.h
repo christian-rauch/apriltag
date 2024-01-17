f65dc38aa4d3c9833eaf78922331c0a231a38807
Christian Rauch <Rauch.Christian@gmx.de>
2024-01-17 07:43:37 +0100

    [WIP] generate export header

--- common/pthreads_cross.h
+++ common/pthreads_cross.h
@@ -31,6 +31,7 @@ SOFTWARE.
 #include <sched.h>
 #endif
 #include <time.h>
+#include <apriltag_export.h>
 
 #ifdef _WIN32
 
@@ -45,23 +46,23 @@ typedef CONDITION_VARIABLE pthread_cond_t;
 #ifdef __cplusplus
 extern "C" {
 #endif
-int pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
-int pthread_join(pthread_t thread, void **value_ptr);
-int pthread_detach(pthread_t);
+APRILTAG_EXPORT int pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+APRILTAG_EXPORT int pthread_join(pthread_t thread, void **value_ptr);
+APRILTAG_EXPORT int pthread_detach(pthread_t);
 
-int pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr);
-int pthread_mutex_destroy(pthread_mutex_t *mutex);
-int pthread_mutex_lock(pthread_mutex_t *mutex);
-int pthread_mutex_unlock(pthread_mutex_t *mutex);
+APRILTAG_EXPORT int pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr);
+APRILTAG_EXPORT int pthread_mutex_destroy(pthread_mutex_t *mutex);
+APRILTAG_EXPORT int pthread_mutex_lock(pthread_mutex_t *mutex);
+APRILTAG_EXPORT int pthread_mutex_unlock(pthread_mutex_t *mutex);
 
-int pthread_cond_init(pthread_cond_t *cond, pthread_condattr_t *attr);
-int pthread_cond_destroy(pthread_cond_t *cond);
-int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
-int pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *abstime);
-int pthread_cond_signal(pthread_cond_t *cond);
-int pthread_cond_broadcast(pthread_cond_t *cond);
+APRILTAG_EXPORT int pthread_cond_init(pthread_cond_t *cond, pthread_condattr_t *attr);
+APRILTAG_EXPORT int pthread_cond_destroy(pthread_cond_t *cond);
+APRILTAG_EXPORT int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
+APRILTAG_EXPORT int pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, const struct timespec *abstime);
+APRILTAG_EXPORT int pthread_cond_signal(pthread_cond_t *cond);
+APRILTAG_EXPORT int pthread_cond_broadcast(pthread_cond_t *cond);
 
-int sched_yield(void);
+APRILTAG_EXPORT int sched_yield(void);
 #ifdef __cplusplus
 }
 #endif
